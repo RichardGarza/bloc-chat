3bb89e076d6e86d66de53336db207093e6d7a6e9
Richard <rgactr@gmail.com>
2019-03-26 15:44:55 -0700

    added a form that enables user to add chatrooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -12,22 +12,46 @@ this.roomsRef = this.props.firebase.database().ref('Rooms');
 }
 
 
-componentDidMount() {
 
-     this.roomsRef.on('child_added', snapshot => {
-       const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) })
-     });
-   }
+
+        componentDidMount() {
+
+             this.roomsRef.on('child_added', snapshot => {
+               const room = snapshot.val();
+               room.key = snapshot.key;
+               this.setState({ rooms: this.state.rooms.concat( room ) })
+             });
+           }
+
+
+           createRoom(event){
+             event.preventDefault();
+
+             const newRoomName = this.refs.newChatName.value;
+             this.roomsRef.push({
+               name: newRoomName
+             });
+             var form = document.getElementById("new-chat-room");
+             form.reset();
+           }
+
 
   render() {
     return (
-    <section> {this.state.rooms.map((e) =>
-      <span key={e.key}>
-        <div> {e.name} </div>
+    <section>
+      <span>
+        {this.state.rooms.map((e) =>
+          <span key={e.key}>
+            <div> {e.name} </div>
+          </span>
+        )}
       </span>
-    )}
+      <form id="new-chat-room" onSubmit={this.createRoom.bind(this)}>
+          New ChatRoom Name: <br/>
+          <input id="chat-room-name" type="text" ref="newChatName"/> <br/>
+
+          <button type="submit"> Add ChatRoom </button>
+     </form>
     </section>
 
     );
